a2ec999badc98769d28b65d575598a772fdb4804
Lucien <fe95789@gmail.com>
2024-04-12 18:27:01 +0800

    feat: husky & vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), tsconfigPaths()],
+  /* UNIT TEST SETUP */
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTest.ts'],
+  },
+})
