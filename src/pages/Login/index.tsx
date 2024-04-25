d6e2d85f4678f0aab8e34d30bfc6dafab615644b
Lucien <fe95789@gmail.com>
2024-04-25 14:33:00 +0800

    modify: import styles at main file

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,9 @@
 function Login() {
-  return <div>Login</div>
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <form className="w-1/4 h-2/3 bg-slate-300 rounded-lg"></form>
+    </main>
+  )
 }
 
 export default Login
