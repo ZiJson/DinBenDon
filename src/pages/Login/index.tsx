7229f0bf8204b173db25dbe0d1b05eed018fb82c
Lucien <fe95789@gmail.com>
2024-04-25 15:32:12 +0800

    feat: basic ui and also add change method button

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,87 @@
+import { useState } from 'react'
+
+type METHOD = 'login' | 'register'
+type BUTTON = 'button' | 'submit'
+
+const fields: Record<METHOD, { id: string; label: string }[]> = {
+  login: [
+    {
+      id: 'account',
+      label: '帳號'
+    },
+    {
+      id: 'password',
+      label: '密碼'
+    }
+  ],
+  register: [
+    {
+      id: 'account',
+      label: '帳號'
+    },
+    {
+      id: 'password',
+      label: '密碼'
+    },
+    {
+      id: 'confirmPassword',
+      label: '確認密碼'
+    }
+  ]
+}
+
+const buttons: Record<METHOD, { id: BUTTON; text: string }[]> = {
+  login: [
+    {
+      id: 'button',
+      text: '前往註冊'
+    },
+    {
+      id: 'submit',
+      text: '登陸'
+    }
+  ],
+  register: [
+    {
+      id: 'button',
+      text: '返回登陸'
+    },
+    {
+      id: 'submit',
+      text: '註冊'
+    }
+  ]
+}
+
 function Login() {
+  const [method, setMethod] = useState<METHOD>('login')
+
   return (
     <main className="w-screen h-screen flex justify-center items-center">
-      <form className="w-1/4 h-2/3 bg-slate-300 rounded-lg"></form>
+      <form className="flex flex-col items-center justify-center gap-10">
+        {fields[method].map(({ id, label }) => (
+          <label key={id}>
+            <p>{label}</p>
+            <input id={id} className="border-2 border-slate-500" type="text" />
+          </label>
+        ))}
+        <ul className="flex gap-10">
+          {buttons[method].map(({ id, text }) => (
+            <li key={id}>
+              <button
+                type={id}
+                className="border-2 border-slate-500"
+                onClick={() =>
+                  id === 'button' &&
+                  setMethod(method === 'login' ? 'register' : 'login')
+                }
+              >
+                {text}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </form>
     </main>
   )
 }
